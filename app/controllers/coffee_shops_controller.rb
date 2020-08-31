b67c73a2f854671babc9ae5a18c63e86167859e0
adrianbunea <adrian_bunea28@yahoo.com>
2020-08-31 13:44:25 +0300

    Add random behaviour for endpoint

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -2,8 +2,28 @@ class CoffeeShopsController < ApplicationController
   def index
     respond_to do |format|
       format.csv do
-        send_file("data/coffee_shops.csv")
+        choose_random_response()
       end
+    end
+  end
+
+  def choose_random_response()
+    ok_response_odds = 5/10.0
+    unauthorized_response_odds = 2/10.0
+    unavailable_response_odds = 2/10.0
+
+    rng = Random.new
+    roll = rng.rand(1.0)
+
+    if roll < ok_response_odds
+      send_file("data/coffee_shops.csv")
+    elsif roll < ok_response_odds + unauthorized_response_odds
+      head :unauthorized
+    elsif roll < ok_response_odds + unauthorized_response_odds + unavailable_response_odds
+      head :service_unavailable
+    else
+      sleep 2.minute
+      head :gateway_timeout
     end
   end
 end
