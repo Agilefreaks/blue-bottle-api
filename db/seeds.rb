7376e8ada44cc3be2a059c2b1cde190aa8a4ce71
pirvudoru <pirvu.doru@ymail.com>
2020-09-01 09:13:10 +0300

    Add coffee shop seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+    {name: 'Blue Bottle Seattle', x: 47.5809, y: -122.3160},
+    {name: 'Blue Bottle SF', x: 37.5209, y: -122.3340},
+    {name: 'Blue Bottle Moscow', x: 55.752047, y: 37.595242},
+    {name: 'Blue Bottle Seattle2', x: 47.5869, y: -122.3368},
+    {name: 'Blue Bottle Rio De Janeiro', x: -22.923489, y: -43.234418},
+    {name: 'Blue Bottle Sydney', x: -33.871843, y: 151.206767}
+].each do |shop|
+  CoffeeShop.find_or_create_by(shop)
+end
