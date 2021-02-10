b8ebc32071013914783ec370505586f23f6511bf
pirvudoru <pirvu.doru@ymail.com>
2021-02-10 15:33:27 +0200

    Add integration tests

--- test/controllers/coffee_shops_controller_test.rb
+++ test/controllers/coffee_shops_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CoffeeShopsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'when no token exists, issues new token' do
+    token = Token.create
+
+    get v1_coffee_shops_path, params: { token: token.value }, headers: { 'Accept' => 'application/json' }
+    assert_equal(200, response.status)
+
+    get v1_coffee_shops_path, params: { token: token.value }, headers: { 'Accept' => 'application/json' }
+    assert_equal(200, response.status)
+
+    get v1_coffee_shops_path, params: { token: token.value }, headers: { 'Accept' => 'application/json' }
+    assert_equal(200, response.status)
+
+    get v1_coffee_shops_path, params: { token: token.value }, headers: { 'Accept' => 'application/json' }
+    assert_equal(503, response.status)
+  end
 end
